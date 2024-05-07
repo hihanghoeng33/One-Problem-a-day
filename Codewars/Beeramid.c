b8387b223ea8d581b92abde66b7fc517dd351879
Ardanatha <142950868+hihanghoeng33@users.noreply.github.com>
2024-05-07 15:07:38 +0700

    Create Beeramid.c

---
+++ Codewars/Beeramid.c
@@ -0,0 +1,26 @@
+/*Problems
+Let's pretend your company just hired your friend from college and paid you a referral bonus. Awesome! To celebrate, you're taking your team out to the terrible dive bar next door and using the referral bonus to buy, and build, the largest three-dimensional beer can pyramid you can. And then probably drink those beers, because let's pretend it's Friday too.
+
+A beer can pyramid will square the number of cans in each level - 1 can in the top level, 4 in the second, 9 in the next, 16, 25...
+
+Complete the beeramid function to return the number of complete levels of a beer can pyramid you can make, given the parameters of:
+
+1. your referral bonus, and
+
+2. the price of a beer can
+
+For example:
+
+beeramid(1500, 2); // should === 12
+beeramid(5000, 3); // should === 16
+*/
+int beeramid(double bonus, double price) {
+    int res = bonus / price;
+    int n = 1;
+    int level = 0;
+    while (level <= res) {
+        level = (n * (n + 1) * (2 * n + 1)) / 6; 
+        n++;
+    }
+    return n - 2; 
+}
