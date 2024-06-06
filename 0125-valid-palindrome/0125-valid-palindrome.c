45e3ec2ea7d00692403cedb391ac9217ab4be662
Ardanatha <142950868+hihanghoeng33@users.noreply.github.com>
2024-06-06 08:29:29 +0700

    Time: 0 ms (100%), Space: 6.5 MB (5.59%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.c
@@ -0,0 +1,19 @@
+bool isPalindrome(char* s) {
+    int len = strlen(s);
+    int k=0;
+    char *newS=(char*)malloc((len+1)*sizeof(char));
+    for(int i=0;i<len;i++){
+        if(isalpha(s[i])||(s[i]<='9' && s[i]>='0')){
+            newS[k++] = tolower(s[i]);
+        }
+    }
+    int i = k-1, j=0;
+    while(i>j){
+        if(newS[i]!=newS[j]){
+            return false;
+        }
+        i--;
+        j++;
+    }
+    return true;
+}
