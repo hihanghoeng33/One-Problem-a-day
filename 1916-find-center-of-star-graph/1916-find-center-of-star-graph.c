fb1874deb3c9abb49635714808072e495ceb69d7
Ardanatha <142950868+hihanghoeng33@users.noreply.github.com>
2024-06-27 07:23:09 +0700

    Time: 156 ms (22.35%), Space: 25.8 MB (11.17%) - LeetHub

--- 1916-find-center-of-star-graph/1916-find-center-of-star-graph.c
+++ 1916-find-center-of-star-graph/1916-find-center-of-star-graph.c
@@ -1,11 +1,9 @@
 int findCenter(int** edges, int edgesSize, int* edgesColSize) {
-    // The center node will appear in both of the first two edges.
     int node1 = edges[0][0];
     int node2 = edges[0][1];
     int node3 = edges[1][0];
     int node4 = edges[1][1];
 
-    // The center node is the one that appears in both edges.
     if (node1 == node3 || node1 == node4) {
         return node1;
     } else {
