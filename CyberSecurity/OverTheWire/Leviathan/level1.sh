e1c9c2c9b74a63a62cca89a8d020de9d38b8b9a0
Ardanatha <142950868+hihanghoeng33@users.noreply.github.com>
2024-08-01 18:51:44 +0700

    Create level1.sh

---
+++ CyberSecurity/OverTheWire/Leviathan/level1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+```sshpass -p 3QJ3TgzHDq ssh leviathan1@leviathan.labs.overthewire.org -p 2223```
+
+
+```ls -la```
+
+
+```ltrace ./check```
+
+```cd /etc/leviathan_pass```
+
+```cat leviathan2```
