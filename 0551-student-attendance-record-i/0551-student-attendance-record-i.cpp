7384b06cc715a8af82118176a658b4421b94d98e
Ardanatha <142950868+hihanghoeng33@users.noreply.github.com>
2024-08-08 12:34:01 +0700

    Time: 4 ms (18.07%), Space: 7.8 MB (17.19%) - LeetHub

--- 0551-student-attendance-record-i/0551-student-attendance-record-i.cpp
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
     bool checkRecord(string s) {
-        int countA=0, countL=0, checkL=1;
-        int eligible = 1;
-        for(char c:s){
-            if(c=='L'){
-                if(checkL==1){
-                    countL++;
-                }else{
-                    checkL=1;
-                    countL=1;
-                }
-                if(countL>=3) return false;
-            }else{
-                checkL=0;
-                if(c=='A'){
-                    countA++;
-                }
+        int countA = 0, countL = 0;
+
+        for (char c : s) {
+            if (c == 'A') {
+                countA++;
+                if (countA >= 2) return false;
+                countL = 0; 
+            } else if (c == 'L') {
+                countL++;
+                if (countL >= 3) return false;
+            } else {
+                countL = 0;
             }
-            cout << countA << " " << countL << endl;
         }
-        
-        if(countA>=2 || countL>=3){eligible = 0;}
-        return eligible==1;
+
+        return true;
     }
 };
